28128f981beb7c095eecea19ffe810fd332d2bce
wendy <874493907@qq.com>
2021-09-02 13:52:33 +0800

    js实现二分查找

--- Leetcode/easy/code704.js
+++ Leetcode/easy/code704.js
@@ -6,14 +6,22 @@
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 */
 var search = function(nums, target) {
+    //1. 找到始末两区间
     let left = 0, right =  nums.length-1
+    //2. 循环遍历的终止条件，当两指针指向同一位置时代表遍历结束
     while(left <= right) {
+        //3. 位运算找到中间值
         let mid = (left + right) >> 1
-        if(nums[mid] === target) 
-        return mid
-        let isSmall = nums[mid] < target
-        left = isSmall ? mid + 1 : 1
-        right= isSmall ? r : mid - 1
+        //4. 判断中间值与目标值大小，相等即返回
+        if(nums[mid] === target) {
+            return mid
+        //5. 判断中间值和目标值大小从而缩小区间，中<目，左区间变为中后面一个
+        }else if (nums[mid] < target) {
+            left = mid + 1
+        //6. 中>目，右区间变为中前面一个
+        }else {
+            right = mid - 1
+        }
     }   
     return -1
-}
+};
