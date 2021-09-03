b00b678a72a46dd4e0444eee8ed62af0e48ed0c0
DayDayUpDeng <13158851157@163.com>
2021-09-03 23:12:14 +0800

    嘿嘿

--- LeetCode/easy/Code997.js
+++ LeetCode/easy/Code997.js
@@ -35,5 +35,38 @@ var sortedSquares = function(nums) {
 
 /**
  * 官方解答--双指针
+ * 数组其实是有序的， 只不过负数平方之后可能成为最大数了。
+
+那么数组平方的最大值就在数组的两端，不是最左边就是最右边，不可能是中间。
+
+此时可以考虑双指针法了，i指向起始位置，j指向终止位置。
+
+定义一个新数组result，和A数组一样的大小，让k指向result数组终止位置。
+
+如果A[i] * A[i] < A[j] * A[j] 那么result[k--] = A[j] * A[j]; 。
+
+如果A[i] * A[i] >= A[j] * A[j] 那么result[k--] = A[i] * A[i]; 。
+
  */
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ var sortedSquares = function(nums) {
+    let res = []
+    for(let i =0 ,j = nums.length-1 ;i<=j;){
+        const left = Math.abs(nums[i]);
+        const right = Math.abs(nums[j])
+
+        if(right > left){
+            res.unshift(right * right)
+            j--;
+        } else{
+            res.unshift(left * left)
+            i++;
+        }
+    }
+    return res;
+
+};
 
