d9f31dd64bc74c63dbe80d0fe6c9d8244c171897
star <qinggang372422@gmail.com>
2021-09-03 21:32:35 +0800

    搜索插入位置

--- Leetcode/easy/src/Code278.java
+++ Leetcode/easy/src/Code278.java
@@ -1,9 +1,21 @@
-import VersionControl;
-public class Code278 extends VersionControl{
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         int low = 1;
         int high = n;
+        int mid = 0;
+        while(low<high){
+            mid = low + (high - low)/2;
+            if(!isBadVersion(mid)){
+                low = mid+1;
 
+            }
+            if(isBadVersion(mid)){
+                high = mid;
+            }
+        }
+        return low;
     }
-
 }
